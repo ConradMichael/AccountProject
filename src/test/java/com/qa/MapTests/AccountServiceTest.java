303897faaafa6e98963f5f31471a8df1b90af3b1
ConradMichael <conrad-michael@outlook.com>
2019-01-29 15:35:18 +0000

    Updating Tests

--- src/test/java/com/qa/MapTests/AccountServiceTest.java
+++ src/test/java/com/qa/MapTests/AccountServiceTest.java
@@ -30,11 +30,13 @@ public class AccountServiceTest {
 		String accountString = js.getJSONForObject(newAccount);
 		acc.createAccount(accountString);
 		Account newAccount1 = js.getObjectForJSON(accountString, Account.class);
+		
+		assertEquals("1", newAccount.getAccountNumber().toString());
 	}
 
 	@Test
 	public void add2AccountTest() {
-		Account newAccount = new Account("First Name","Last Name", 2);
+		Account newAccount = new Account("First Name","Last Name", 1);
 		Account newAccount2 = new Account("First Name","Last Name", 2);
 		
 		String accountString = js.getJSONForObject(newAccount);
@@ -43,8 +45,12 @@ public class AccountServiceTest {
 		acc.createAccount(accountString);
 		acc.createAccount(accountString2);
 		
-		Account newAccount1 = js.getObjectForJSON(accountString, Account.class);
-		Account newAccount3 = js.getObjectForJSON(accountString, Account.class);
+		newAccount = js.getObjectForJSON(accountString, Account.class);
+		newAccount2 = js.getObjectForJSON(accountString2, Account.class);
+		
+
+		assertEquals("1", newAccount.getAccountNumber().toString());
+		assertEquals("2", newAccount2.getAccountNumber().toString());
 	}
 	
 	@Test
@@ -69,7 +75,7 @@ public class AccountServiceTest {
 		Account newAccount2 = new Account("First Name","Last Name", 2);
 		
 		String accountString = js.getJSONForObject(newAccount);
-		String accountString2 = js.getJSONForObject(newAccount2);
+		String accountString2 = js.getJSONForObject(newAccount2);  
 		
 		acc.createAccount(accountString);
 		acc.createAccount(accountString2);
